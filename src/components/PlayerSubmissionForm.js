2c4b7aeda845abd72db04dd4b39ee83408bcefc5
Iris Lux <iris.lux0@gmail.com>
2021-01-07 19:46:04 -0800

    displays most recent submission

--- src/components/PlayerSubmissionForm.js
+++ src/components/PlayerSubmissionForm.js
@@ -3,26 +3,75 @@ import PropTypes from 'prop-types';
 
 import './PlayerSubmissionForm.css';
 
-const PlayerSubmissionForm = () => {
+const PlayerSubmissionForm = (props) => {
+
+  const [formFields, setFormFields] = useState({
+    adj1: '',
+    noun1: '',
+    adv: '',
+    verb: '',
+    adj2: '', 
+    noun2: ''
+  });
+
+  const onInputChange = (event) => {
+    const newFormFields = {
+      ...formFields,
+    }
+    newFormFields[event.target.name] = event.target.value;
+    setFormFields(newFormFields);
+  }
+
+  const formInputs = (
+    props.fields.map(field => {
+      if(typeof field === 'string'){
+        return field 
+      }
+
+      else{
+        return (
+          <input className= {formFields[field.key] === '' ? 'PlayerSubmissionFormt__input--invalid' : 'PlayerSubmissionForm__poem-input' }
+          placeholder = {field.placeholder}
+          onChange = {onInputChange}
+          value = {formFields[field.key]}
+          name = {field.key}
+        />
+        ) 
+      }
+    })
+  )
+
+
+  const onFormSubmit = (event) => {
+    event.preventDefault();
+    console.log('hhh')
+    props.sendSubmission(formFields);
+
+    setFormFields({
+      adj1: '',
+      noun1: '',
+      adv: '',
+      verb: '',
+      adj2: '', 
+      noun2: ''
+    });
+  };
+  
+  const outputForm = () => {
+
+  }
   return (
     <div className="PlayerSubmissionForm">
       <h3>Player Submission Form for Player #{  }</h3>
 
-      <form className="PlayerSubmissionForm__form" >
+      <form className="PlayerSubmissionForm__form" onSubmit = {onFormSubmit}>
 
         <div className="PlayerSubmissionForm__poem-inputs">
-
-          {
-            // Put your form inputs here... We've put in one below as an example
-          }
-          <input
-            placeholder="hm..."
-            type="text" />
-
+          {formInputs}
         </div>
 
         <div className="PlayerSubmissionForm__submit">
-          <input type="submit" value="Submit Line" className="PlayerSubmissionForm__submit-btn" />
+          <input type="submit" value="Submit Line" className="PlayerSubmissionForm__submit-btn"/>
         </div>
       </form>
     </div>
