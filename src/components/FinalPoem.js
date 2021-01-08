96273c8a4f1e2d65ff9aa0b724ca51c79bff12e7
Iris Lux <iris.lux0@gmail.com>
2021-01-07 19:45:17 -0800

    onButtonCLick

--- src/components/FinalPoem.js
+++ src/components/FinalPoem.js
@@ -1,18 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import './FinalPoem.css';
 
 const FinalPoem = (props) => {
 
+  const [finalSentence, setFinalSentence] = useState('')
+
+  const onButtonClick = () => {
+    setFinalSentence(props.revealPoem())
+  }
+
   return (
     <div className="FinalPoem">
       <section className="FinalPoem__poem">
         <h3>Final Poem</h3>
-
+            {finalSentence}
       </section>
 
       <div className="FinalPoem__reveal-btn-container">
-        <input type="button" value="We are finished: Reveal the Poem" className="FinalPoem__reveal-btn" />
+        <input type="button" value="We are finished: Reveal the Poem" className="FinalPoem__reveal-btn" onClick = {onButtonClick} />
       </div>
     </div>
   );
